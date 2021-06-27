8de7d8488d2824f1c278ed047b8298a9462a18cd
laviiiii <d68423@gmail.com>
2021-06-28 03:59:42 +0800

    fix requirement.txt

--- mylinebot/settings.py
+++ mylinebot/settings.py
@@ -142,5 +142,5 @@ LINE_CHANNEL_SECRET = '14af0389de1592bc024dff62c9da8c99'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8000))
     app.run(debug=True, host='0.0.0.0', port=port)
