38d213b27f4ad98325bd8a078548c93d0a616fc0
laviiiii <d68423@gmail.com>
2021-06-28 03:51:09 +0800

    fix default language

--- mylinebot/settings.py
+++ mylinebot/settings.py
@@ -140,3 +140,7 @@ LINE_CHANNEL_SECRET = '14af0389de1592bc024dff62c9da8c99'
 
 #static root
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
